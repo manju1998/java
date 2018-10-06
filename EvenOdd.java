374b797297567b2f6d95a4eda9cdca52342748e4
manju1998 <43902301+manju1998@users.noreply.github.com>
2018-10-06 14:56:20 +0530

    Add files via upload

--- EvenOdd.java
+++ EvenOdd.java
@@ -8,10 +8,10 @@ class EvenOdd {
 
         
         int num = reader.nextInt();
-        if(num <0)
-            System.out.println(num + " is invalid");
 
-        if(num % 2 == 0)
+	if(num < 0)
+	System.out.println(num + " is invalid");
+        else if(num % 2 == 0)
             System.out.println(num + " is even");
         else
             System.out.println(num + " is odd");
